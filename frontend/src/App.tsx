ca730d0ca7a10370b8cf922ff16b46998dff3da1
LiamBoz <thisamazingnow@gmail.com>
2025-03-15 23:58:23 -0400

    map centered to gainesville, added marker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, Marker, useMap } from '@vis.gl/react-google-maps';
 
 const API_KEY = '';
 
@@ -7,17 +7,28 @@ const API_KEY = '';
 function App() {
   console.log('App component rendering');
   console.log('API Key:', API_KEY);
- 
+
+  const test_location  = [
+    {lat: 29.6465, lng: 82.3533, title: 'test'},
+  ];
   return (
     <div className="map-container">
       <APIProvider apiKey={API_KEY}>
         <Map
-          style={{width: '20%', height: '20%'}}
-          defaultZoom={10}
-          defaultCenter={{lat: 22.54992, lng: 0}}
+          style={{width: '25%', height: '50%'}}
+          defaultZoom={5}
+          defaultCenter={{lat: 29.6465, lng: 82.3533}}
           gestureHandling={'greedy'}
           disableDefaultUI={true}
-        />
+        >
+          {test_location.map((location, index) => (
+            <Marker 
+              key={index}
+              position={location}
+              title={location.title}
+            />
+          ))}
+        </Map>
       </APIProvider>
     </div>
   );
