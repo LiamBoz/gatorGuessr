5130810ef7772e8320e323ffe0fdd1055f9ce8c8
Tavian Douge <taviandouge@gmail.com>
2025-04-05 21:07:24 -0400

    Adjusted ui, moved marker to uf

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,29 +9,39 @@ function App() {
   console.log('API Key:', API_KEY);
 
   const test_location  = [
-    {lat: 29.6465, lng: 82.3533, title: 'test'},
+    {lat: 29.6465, lng: -82.3533, title: 'test'},
   ];
   return (
-    <div className="map-container">
-      <APIProvider apiKey={API_KEY}>
-        <Map
-          style={{width: '25%', height: '50%'}}
-          defaultZoom={5}
-          defaultCenter={{lat: 29.6465, lng: 82.3533}}
-          gestureHandling={'greedy'}
-          disableDefaultUI={true}
-        >
-          {test_location.map((location, index) => (
-            <Marker 
-              key={index}
-              position={location}
-              title={location.title}
-            />
-          ))}
-        </Map>
-      </APIProvider>
+    <div className="game-container">
+      <header className="header">Gator Guessr</header>
+  
+      <div className="photo-container">
+        {/* image path */}
+        <div className="photo-placeholder">Photo goes here</div>
+      </div>
+  
+      <div className="map-wrapper">
+        <APIProvider apiKey={API_KEY}>
+          <Map
+            style={{ width: '100%', height: '100%', borderRadius: '12px' }}
+            defaultZoom={15}
+            defaultCenter={{ lat: 29.6465, lng: 82.3533 }}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}
+          >
+            {test_location.map((location, index) => (
+              <Marker
+                key={index}
+                position={location}
+                title={location.title}
+              />
+            ))}
+          </Map>
+        </APIProvider>
+      </div>
     </div>
   );
+  
 }
 
 export default App;
