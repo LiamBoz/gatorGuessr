38faa01998101200274c419e28b4b1ece8728e1d
LiamBoz <thisamazingnow@gmail.com>
2025-07-02 23:58:38 -0400

    .

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-engine = create_engine("", echo=True)
+engine = create_engine("postgresql+psycopg2://admin:password@localhost:5432/postgres", echo=True)
 
 
