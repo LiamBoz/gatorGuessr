f2be2661ca1f6d7afc8277e3b3a43b3c79295e01
LiamBoz <thisamazingnow@gmail.com>
2025-07-05 22:00:30 -0400

    models done

--- backend/models/models.py
+++ backend/models/models.py
@@ -1,18 +1,31 @@
 from database.base import Base
-from sqlalchemy import String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from sqlalchemy import Float, String, Column, Integer
 
 class ImageTable(Base):
     __tablename__ = "images"
 
-    id: int = mapped_column(primary_key=True)
-    title: str = column()
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    url = Column(String)
+    created_at = Column(String)
 
 class EntryTable(Base):
     __tablename__ = "entries"
+    id = Column(Integer, primary_key=True)
+    image_id = Column(Integer)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    likes = Column(Integer)
+    dislikes = Column(Integer)
+    created_at = Column(String)
 
 class GuessTable(Base):
     __tablename__ = "guesses"
+    id = Column(Integer, primary_key=True)
+    entry_id = Column(Integer)
+    guessed_longitude = Column(Float)
+    guessed_latitude = Column(Float)
+    distance = Column(Float)
+    score = Column(Integer)
+    created_at = Column(String)
+
